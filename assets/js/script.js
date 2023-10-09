7a3a6585ec4e0ea46068fabdef20e5c7f95279ea
MAT-2 <mpcasey30@gmail.com>
2023-10-09 14:45:22 -0500

    Added new card. Added two variables, one empty variable and an APIKey. Added the query URL for celebrity name API, used their documentation in order to use API Key, and console.log to test.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
 var celebrityInputEl = document.querySelector("#celebrity-input");
 var submitNameEl = document.querySelector("#submit-name-button");
 var time = dayjs();
+var name;
+var APIKey = "oEDDv6v6z7pbtqFgBK1DQQ==U8mDL9ZgMIL4xrIy";
 
 $("#presentDay").text(time.format("dddd, MMM D, YYYY"));
 
 function celebrityAPI() {
   var name = celebrityInputEl.value;
   console.log(name);
+
+  var celebrityURL = `https://api.api-ninjas.com/v1/celebrity?name=${name}`;
+  var options = {
+    method: "GET",
+    headers: { "X-Api-Key": APIKey },
+    contentType: "application/json",
+  };
+  fetch(celebrityURL, options)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      //Have to access data array and use index numbers to access object. Helps access specific data.
+      console.log(data[0].age);
+    });
 }
 
 submitNameEl.addEventListener("click", celebrityAPI);
