eb0fef12ccc45ee6c4fbb829cd47d4ca040465ac
MAT-2 <mpcasey30@gmail.com>
2023-10-09 14:09:03 -0500

    Added IDs to take user input for celebrity name and click event for button clicked. Added variables to collect the input, and tested function celebrityAPI() via console.log.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
+var celebrityInputEl = document.querySelector("#celebrity-input");
+var submitNameEl = document.querySelector("#submit-name-button");
 var time = dayjs();
+
 $("#presentDay").text(time.format("dddd, MMM D, YYYY"));
+
+function celebrityAPI() {
+  var name = celebrityInputEl.value;
+  console.log(name);
+}
+
+submitNameEl.addEventListener("click", celebrityAPI);
